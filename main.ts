c9052f40da7cfef0b7470612c7f76e92cf09e655
FX918 <72587476+FX918@users.noreply.github.com>
2021-05-02 09:17:10 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,36 +6,9 @@ function 右轉 () {
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
 }
-input.onButtonPressed(Button.A, function () {
-    右轉()
-    basic.showIcon(IconNames.Scissors)
-})
 function 後退 () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
 }
-input.onButtonPressed(Button.AB, function () {
-    後退()
-    basic.showIcon(IconNames.Tortoise)
-})
-input.onButtonPressed(Button.B, function () {
-    左轉()
-    basic.showIcon(IconNames.House)
-})
 function 直線 () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
 }
-basic.forever(function () {
-    for (let index = 0; index < 4; index++) {
-        basic.showIcon(IconNames.Heart)
-        右轉()
-        basic.pause(1000)
-        basic.showIcon(IconNames.Happy)
-        直線()
-        basic.showIcon(IconNames.StickFigure)
-        basic.pause(1000)
-        左轉()
-        basic.showIcon(IconNames.Yes)
-        直線()
-        basic.pause(1000)
-    }
-})
